0af2dedc71604074a3177c5053ab9e5eefab1a4c
masoug <masoug@gmail.com>
2017-04-28 10:22:56 -0700

    formatting and non-initial import

--- sim.py
+++ sim.py
@@ -1,41 +1,53 @@
-# GLOBALS:
-weight_memory = np.load('dram.npy')
-host_memory = np.load('hostmem.npy')
-unified_buffer = np.zeros((96000, 256))
-accumulator = np.zeros((4000, 256))
-weight_fifo = []
+import sys
+import numpy as np
 
-# read text file with program
-with open(sys.argv[1]) as f:
-	for line in f:
-		v = line.split(" ")
-		opcode = v[0]
-		operands = v[1:]
 
-# opcodes
-def read_host_mem(host_start_addr, ub_start_addr, size):
-	'''
-	Reads a range of memory addresses from "start_addr" to "stop_addr" in the
-	host_memory
-	'''
-	unified_buffer[ub_start_addr:ub_start_addr+size] = host_memory[host_start:host_start+size]
-	print("Host Memory Read Complete")
+class TPUSim(object):
+    def __init__(self, program_filename, dram_filename, hostmem_filename):
+        self.program = open(program_filename, 'rb')
+        self.weight_memory = np.load(dram_filename)
+        self.host_memory = np.load(hostmem_filename)
+        self.unified_buffer = np.zeros((96000, 256))
+        self.accumulator = np.zeros((4000, 256))
+        self.weight_fifo = []
 
-def write_host_mem(ub_start_addr, host_start, size):
-	'''
-	Writes "data" to a range of memory addresses from "start_addr" to
-	"stop_addr" in host_memory
-	'''
-	host_memory[host_start:host_start+size] = unified_buffer[ub_start_addr:ub_start_addr+size]
-	print("Host Memory Write Complete")
+    def run(self):
+        # load program and execute instructions
+        self.program.close()
 
-def read_weights(weight_dram_addr):
-	'''
-	Moves one tiles into the weight fifo
-	'''
-	weight_fifo.append(weight_memory[weight_dram_addr])
+    # opcodes
+    def read_host_mem(self, host_addr, ub_addr, size):
+        """
+        Reads a range of memory addresses from "start_addr" to "stop_addr" in the
+        host_memory
+        """
+        self.unified_buffer[ub_addr:ub_addr + size] = self.host_memory[host_addr:host_addr + size]
+        print("Host Memory Read Complete")
 
-def matrix_multiply_convolve(switch, ub_start_addr, accumulator_addr, size, weight_fifo):
-	inp = unified_buffer[ub_start_addr: ub_start_addr+size]
-	out = np.matmul(inp, weight_fifo.pop())
-	accumulator[accumulator_addr:accumulator_addr+256] = out# because matmul unit is 256*256
+    def write_host_mem(self, ub_addr, host_addr, size):
+        """
+        Writes "data" to a range of memory addresses from "start_addr" to
+        "stop_addr" in host_memory
+        """
+        self.host_memory[host_addr:host_addr + size] = self.unified_buffer[ub_addr:ub_addr + size]
+        print("Host Memory Write Complete")
+
+    def read_weights(self, weight_dram_addr):
+        """
+        Moves one tiles into the weight fifo
+        """
+        self.weight_fifo.append(self.weight_memory[weight_dram_addr])
+
+    def matrix_multiply_convolve(self, switch, ub_addr, accumulator_addr, size, weight_fifo):
+        inp = self.unified_buffer[ub_addr: ub_addr + size]
+        out = np.matmul(inp, weight_fifo.pop())
+        self.accumulator[accumulator_addr:accumulator_addr + 256] = out  # because matmul unit is 256*256
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 4:
+        print 'Usage:', sys.argv[0], 'PROGRAM_BINARY DRAM_FILE HOST_FILE'
+        sys.exit(0)
+
+    tpusim = TPUSim(sys.argv[1], sys.argv[2], sys.argv[3])
+    tpusim.run()
