8f222ac623dce407475a8a5ab11ea41d7604e55d
David Coffill <decoffill@gmail.com>
2017-04-28 13:56:10 -0700

    Made assembler easier to understand, added overwrite flag

--- assembler.py
+++ assembler.py
@@ -31,8 +31,8 @@ EXAMPLES:
 
 INST is encoded in a little-endian format.
 OPCODE values are defined in OPCODE2BIN.
-FLAG field is r|r|r|r|r|r|s|c, r stands for reserved bit, s for switch bit,
-c for convolve bit.
+FLAG field is r|r|r|r|r|o|s|c, r stands for reserved bit, s for switch bit,
+c for convolve bit, and o for override bit.
 
 SRC and TAR are addresses. They can be of variable length defined in
 global dict OPCODE2BIN.
@@ -50,19 +50,26 @@ args = None
 
 # Map text opcode to instruction decomposition info.
 # Str -> (opcode_value, src_len, tar_len, 3rd_len)
+
+HOST_ADDR_SIZE = 8 # 64-bit addressing
+DRAM_ADDR_SIZE = 5 # 33-bit addressing (TPU has 8 GB on-chip DRAM)
+UB_ADDR_SIZE = 3 # 17-bit addressing for Unified Buffer
+ACC_ADDR_SIZE = 2 # 12-bit addressing for accumulator
+
 OPCODE2BIN = {
-        'RHM': (0x0, 8, 3, 1),
-        'WHM': (0x1, 3, 8, 1),
-        'RW': (0x2, 5, 0, 0),
-        'MMC': (0x3, 3, 2, 2),
-        'ACT': (0x4, 2, 3, 1),
+        'RHM':  (0x0, HOST_ADDR_SIZE, UB_ADDR_SIZE,   1),
+        'WHM':  (0x1, UB_ADDR_SIZE,   HOST_ADDR_SIZE, 1),
+        'RW':   (0x2, DRAM_ADDR_SIZE, 0,              0),
+        'MMC':  (0x3, UB_ADDR_SIZE,   ACC_ADDR_SIZE,  2),
+        'ACT':  (0x4, ACC_ADDR_SIZE,  UB_ADDR_SIZE,              1),
         'SYNC': (0x5, 0, 0, 0),
-        'NOP': (0x6, 0, 0, 0),
-        'HLT': (0x7, 0, 0, 0),
+        'NOP':  (0x6, 0, 0, 0),
+        'HLT':  (0x7, 0, 0, 0),
         }
 
 SWITCH_MASK = 0x1
 CONV_MASK = 0x2
+OVERWRITE_MASK = 0x4 # whether MMC should overwrite accumulator value or add to it
 
 TOP_LEVEL_SEP = re.compile(r'[a-zA-Z]+\s+')
 
@@ -111,6 +118,8 @@ def assemble(path, n):
             flag |= SWITCH_MASK
         if 'C' in flags:
             flag |= CONV_MASK
+        if 'O' in flags:
+            flag |= OVERWRITE_MASK
 
         # binary for flags
         bin_flags = flag.to_bytes(1, byteorder='little')
